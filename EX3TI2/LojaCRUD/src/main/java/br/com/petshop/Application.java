f9d8292921fb4eaf22a0bc9f626e17e9bf45f5ac
Viannxo <eduardo.vmeira@gmail.com>
2025-09-25 17:43:46 -0300

    edit

--- EX3TI2/LojaCRUD/src/main/java/br/com/petshop/Application.java
+++ EX3TI2/LojaCRUD/src/main/java/br/com/petshop/Application.java
@@ -2,43 +2,52 @@ package br.com.petshop;
 
 import br.com.petshop.service.ClienteService;
 import br.com.petshop.service.PetService;
+
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.*;
 
-public class Aplicacao {
+public class Application {
 
     public static void main(String[] args) {
-        port(4567);
-        staticFiles.location("/public");
-        
+        port(4567); 
+        staticFiles.location("\\src\\main\\resources\\templates");
+
         System.out.println("Servidor iniciado. Acesse o link: http://localhost:4567/cliente/list");
         System.out.println("Pressione Ctrl + C para encerrar o servidor.");
 
         ClienteService clienteService = new ClienteService();
         PetService petService = new PetService();
 
-        // Rotas para Clientes
+        // Página inicial redireciona para listagem de clientes
+        get("/", (req, res) -> {
+            res.redirect("/cliente/list");
+            return null;
+        });
+
+        // Rotas Cliente
         get("/cliente/list", clienteService::getListarClientes, new FreeMarkerEngine());
-        get("/cliente/add", (request, response) -> new ModelAndView(null, "addCliente.html"), new FreeMarkerEngine());
+        get("/cliente/add", (req, res) -> new ModelAndView(null, "addCliente.html"), new FreeMarkerEngine());
         post("/cliente/save", clienteService::inserirCliente);
         get("/cliente/edit/:id", clienteService::getEditarCliente, new FreeMarkerEngine());
         post("/cliente/update/:id", clienteService::atualizarCliente);
         get("/cliente/delete/:id", clienteService::deletarCliente);
 
-        // Rotas para Pets
+        // Rotas Pet
         get("/pet/list/:clienteId", petService::getListarPetsDoCliente, new FreeMarkerEngine());
-        get("/pet/add/:clienteId", (request, response) -> {
+        get("/pet/add/:clienteId", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("clienteId", request.params(":clienteId"));
+            model.put("clienteId", req.params(":clienteId"));
             return new ModelAndView(model, "addPet.html");
         }, new FreeMarkerEngine());
         post("/pet/save", petService::inserirPet);
-        get("/pet/edit/:id", (request, response) -> {
+        get("/pet/edit/:id", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
+            model.put("id", req.params(":id"));
             return new ModelAndView(model, "editPet.html");
         }, new FreeMarkerEngine());
         post("/pet/update/:id", petService::atualizarPet);
